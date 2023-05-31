0d9244ce44005aeba00aeb1a995e0d2178677ad5
jphwang212 <jphwang212@gmail.com>
2023-05-30 20:09:29 -0400

    completed assignments

--- src/ClosestQuakes.java
+++ src/ClosestQuakes.java
@@ -12,20 +12,30 @@ public class ClosestQuakes {
     public ArrayList<QuakeEntry> getClosest(ArrayList<QuakeEntry> quakeData, Location current, int howMany) {
         ArrayList<QuakeEntry> ret = new ArrayList<QuakeEntry>();
         // TO DO
-
+        for(int i = 0; i < howMany; i++){
+            int closestQuakeIdx = 0;
+            for(int j = 0; j < quakeData.size(); j++){
+                double distance = current.distanceTo(quakeData.get(j).getLocation());
+                if(distance < current.distanceTo(quakeData.get(closestQuakeIdx).getLocation())){
+                    closestQuakeIdx = j;
+                }
+            }
+            ret.add(quakeData.get(closestQuakeIdx));
+            quakeData.remove(closestQuakeIdx);
+        }
         return ret;
     }
 
     public void findClosestQuakes() {
         EarthQuakeParser parser = new EarthQuakeParser();
-        String source = "data/nov20quakedata.atom";
+        String source = "data/nov20quakedatasmall.atom";
 //        String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
         ArrayList<QuakeEntry> list  = parser.read(source);
         System.out.println("read data for "+list.size());
 
         Location jakarta  = new Location(-6.211,106.845);
 
-        ArrayList<QuakeEntry> close = getClosest(list,jakarta,10);
+        ArrayList<QuakeEntry> close = getClosest(list,jakarta,3);
         for(int k=0; k < close.size(); k++){
             QuakeEntry entry = close.get(k);
             double distanceInMeters = jakarta.distanceTo(entry.getLocation());
---
+++ src/LargestQuakes.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class LargestQuakes {
+    public void findLargestQuakes(){
+        EarthQuakeParser parser = new EarthQuakeParser();
+        String source = "data/nov20quakedatasmall.atom";
+//        String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        System.out.println("read data for "+list.size());
+        ArrayList<QuakeEntry> largestQuakes = getLargest(list, 5);
+        for(QuakeEntry qe : largestQuakes){
+            System.out.println(qe);
+        }
+    }
+    public int indexOfLargest(ArrayList<QuakeEntry> data){
+        int idxLargest = 0;
+        double largestMag = 0;
+        for(int i = 0; i < data.size(); i++){
+            if(data.get(i).getMagnitude() > largestMag){
+                largestMag = data.get(i).getMagnitude();
+                idxLargest = i;
+            }
+        }
+        return idxLargest;
+    }
+    public ArrayList<QuakeEntry> getLargest(ArrayList<QuakeEntry> quakeData, int howMany){
+        ArrayList<QuakeEntry> answer = new ArrayList<QuakeEntry>();
+        for(int i = 0; i < howMany; i++){
+            int idxLargest = indexOfLargest(quakeData);
+            answer.add(quakeData.get(idxLargest));
+            quakeData.remove(idxLargest);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        LargestQuakes inst = new LargestQuakes();
+        inst.findLargestQuakes();
+    }
+}
